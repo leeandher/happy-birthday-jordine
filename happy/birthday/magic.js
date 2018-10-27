939b8ac814353ee44f34f6db8b3a44f056be6dca
Leander Rodrigues <leandergrodrigues@gmail.com>
2018-10-27 13:43:39 -0400

    final touches

--- happy/birthday/magic.js
+++ happy/birthday/magic.js
@@ -9,11 +9,6 @@ function swoosh(e) {
   const { offsetWidth: width, offsetHeight: height } = document.body;
   let { screenX: x, screenY: y } = e;
 
-  // if (this !== e.target) {
-  //   x += e.target.offsetLeft;
-  //   y += e.target.offsetTop;
-  // }
-
   const xWalk = (-(x / (width / 2) - 1) * walk * friction).toFixed(2);
   const yWalk = (-(y / (height / 2) - 1) * walk * friction).toFixed(2);
 
