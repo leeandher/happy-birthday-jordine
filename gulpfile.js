25e45b99513635c60a517c8f9df7c660744a0d8b
Leander Rodrigues <leandergrodrigues@gmail.com>
2018-10-26 21:02:09 -0400

    Completed setup!

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const gulp = require("gulp");
+const p = require("gulp-load-plugins")();
+
+const browserSync = require("browser-sync");
+
+// Start the server
+gulp.task("nsync", () => {
+  browserSync({
+    open: true,
+    server: {
+      baseDir: "./happy/birthday/",
+      index: "jordine.html"
+    }
+  });
+});
+
+//Compile pug file
+gulp.task("writin", () => {
+  gulp
+    .src("./dev/iloveevery.pug")
+    .pipe(p.rename("jordine.html"))
+    .pipe(p.pug())
+    .pipe(gulp.dest("./happy/birthday/"))
+    .pipe(browserSync.stream());
+});
+
+//Compile sass file
+gulp.task("stylin", () => {
+  gulp
+    .src("./dev/aesthetics.scss")
+    .pipe(p.sass().on("error", p.sass.logError))
+    .pipe(p.autoprefixer())
+    .pipe(gulp.dest("./happy/birthday"))
+    .pipe(browserSync.stream());
+});
+
+//Default task
+gulp.task("default", ["writin", "stylin", "nsync"], () => {
+  gulp.watch("./dev/iloveevery.pug", ["writin"]);
+  gulp.watch("./dev/aesthetics.scss", ["stylin"]);
+});
